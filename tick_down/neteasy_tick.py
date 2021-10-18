2945f73aab1a5e5c95cb2665bca4292adb50a2e6
Demon Finch <yhf2lj@gmail.com>
2021-10-18 17:36:16 +0800

    修复no try error

--- tick_down/neteasy_tick.py
+++ tick_down/neteasy_tick.py
@@ -32,25 +32,25 @@ class NetTickDown(TickDown):
         while True:
             t1 = datetime.now()
             if self.stock_a_hour(t1):  # 判断A股时间段
-                # try:
-                stkdata = self.formatdata(self.tick_dl(if_thread=True))
-                with open(self.todaycsvpath, mode='a') as file_today:  # 打开文件
-                    writecnt = 0
-                    t3 = datetime.now()
-                    for stki in stkdata:
-                        datanowtime = datetime.strptime(stki["time"], "%Y/%m/%d %H:%M:%S")
-                        if datanowtime > self.stktime[stki['code']]:
-                            # 写入文件
-                            file_today.writelines(
-                                ",".join([str(stki[keysel]) for keysel in self.clname]))
-                            file_today.write("\n")
-                            self.stktime[stki['code']] = datanowtime
-                            writecnt += 1
-                    file_today.close()
-                    t4 = datetime.now()
-                    print(f"localtime: {t4} all:{t4 - t1} tocsv:{t4 - t3} download:{t3 - t1} cnt:{writecnt}")
-                # except Exception as error_downdata:
-                #     print(f"error_downdata: {error_downdata}")
+                try:
+                    stkdata = self.formatdata(self.tick_dl(if_thread=True))
+                    with open(self.todaycsvpath, mode='a') as file_today:  # 打开文件
+                        writecnt = 0
+                        t3 = datetime.now()
+                        for stki in stkdata:
+                            datanowtime = datetime.strptime(stki["time"], "%Y/%m/%d %H:%M:%S")
+                            if datanowtime > self.stktime[stki['code']]:
+                                # 写入文件
+                                file_today.writelines(
+                                    ",".join([str(stki[keysel]) for keysel in self.clname]))
+                                file_today.write("\n")
+                                self.stktime[stki['code']] = datanowtime
+                                writecnt += 1
+                        file_today.close()
+                        t4 = datetime.now()
+                        print(f"localtime: {t4} all:{t4 - t1} tocsv:{t4 - t3} download:{t3 - t1} cnt:{writecnt}")
+                except Exception as error_downdata:
+                    print(f"error_downdata: {error_downdata}")
             elif datetime.now() > self.trd_hour_end_afternoon:  # 下午3：02退出循环
                 print("download complete -> %s" % self.todaycsvpath)
                 break
